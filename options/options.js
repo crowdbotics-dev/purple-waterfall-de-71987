27c325c762ada1ef0c44ef4f2e674e6c9c7bf8a8
Crowdbotics <engineering@crowdbotics.com>
2023-03-25 00:20:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-waterfall-de-71987",
+  url: "https://purple-waterfall-de-71987.botics.co",
+  api: "https://purple-waterfall-de-71987.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
